fdbb55233b188ba6918cec408509597425aaf7df
sakersun <sakersun@163.com>
2023-09-06 15:24:02 +0800

    migrate to next

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import Button from "@mui/material/Button";
+import React from "react";
+
+export default function Home() {
+    return (
+        <Button variant="contained">Hello Next.</Button>
+    )
+}
