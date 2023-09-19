044b6174ba8659b31d9a7fabb72fc6cff26c6afb
sakersun <sakersun@163.com>
2023-09-19 10:23:51 +0800

    clean project

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import Button from "@mui/material/Button";
-import React from "react";
-
-export default function Home() {
-    return (
-        <Button variant="contained">Hello Next.</Button>
-    )
-}
